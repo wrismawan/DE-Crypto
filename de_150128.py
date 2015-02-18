8b9a7f1afe90f462d572430a1e068ccfb22ade69
wrismawan <wahyurismawan@gmail.com>
2015-02-18 21:28:33 +0700

    new

--- de_150128.py
+++ de_150128.py
@@ -5,17 +5,17 @@ import collections as col
 from pyngram import calc_ngram
 
 
-chipperText = 'rtBiihnstilieglneeacecgynCQGhHsoadtyaaulcendhnpapaalcelCydprotthytsaaimdiaetatceighsncnedorsacyolhaoehgicdelnrntahditeercearhaosubcytprortaghWphyliiteaasivallebreflefryaoynneoihwatAdnrnitoadltbtedwonoodlnaAdornipohdnsooeiSrdOvceeispspureodtthaetoemnmbtttuashsthdcueeflodnxrteeryCaytropiryeslyaalmdiaeKytSeaetsguetndsnttieKhGUoirnwfogmrpoajrcdeetinsegfrdCoethelhmnSaincceFsetevlitaephpaeslutesslranearotbbuscaeicynprinttocneihusqaedhnetriishoytarwlsleseatligtynuaovhareacktccaetriagonuyonrewcdneomsdsegsaGeHaCpQBiptrssiphaeyngyCcHGluQnahscceytroprpghaapyfprtousetdsnauNrtlyatledheiitahsthaeteessmaessgrtahenheasevridteauhulscaanhensnldaeirpcetisnaucsnteeahpopttynrdaeidpcethhrhdedineesmaesRggredaeslosweftheohrrotnhtsuedtnseptcuipkseacpfciiitrneetnsciytroprpghainytselrlateyphitohnrieteprstafdoravtioermpooectiSneeTccneohoylEggnneirneginMaadhStTsMoEstuetndsftcOusoerfhietKeUcbmsoeeetvhnsiegltshteimbotewraaefrcoyortpgaoprithechicqneaunscbdeyscrueiyrstilkslhntteashntbdotaighenteirhrklqf'
+chipperText = 'iBirnsihtltiecgnelgenaCcGyehHsQatdoaaynldcehuaapnlclapCeydypotratihmsiataetdhecatgiendcnosyacrlhooseahgridlcaednitehnerctshreaoatbpcyrugtaoWpyhrlhiivtaselabirlfealefenoarynywoaihtennrAtodidlatbotdoelwanAdonornidodhposnrediSOoieevpsuscrpeotdtatmhmebetnotuatctsshdhleoefduertnryaexyCtrsoiyparlemyialaeKdteSyteatgnuedsittsKhUenoGirrnfgwaormcoejpetiddgensrfhoeetlCanShnceimFcseatviehlateplpeuteserlssnabrbotuaiaesynrccipnthtceoqneidsnauetrhthsiiyosrlwlealatsgtnieuyaocvaehcraaetrckiagtryuonnonwecdoeedsmsaegsHGaCrpBpQiihtasypsngyeQGHCculcachsenproypgartahpysfropdtnuatNsertluhltyadeaihitsetaetesmehasesesragetshehranidtvshuealuhaeanncaldsirceneptinsacneuhspepatttyodadnriehchetrpiedheemndasesaRgeglrsdwsfoetheenrrohtoetdsuthtepnuikcsspeaicfipnceitrsetciyngpotrpryatinsharleeyhtliptoehrensiatfrotpravdpeotimrtoiecSoceTnnehceooyliEgngeniennagradhMEsTStoMttnuedsOtcssoruffehibeKUtoeecesvemhnsthlgiestbeoimttaraefroewycorrtgophacphtceiqneidsnaubcryusceetrsilkliyhsntttaheosanidhtbentglhrei'
 #print 'len chip ',len(chipperText)
 
-D = 5 #panjang kunci
+D = 9 
 batas_bawah = 10*D
 batas_atas = 15*D
 #bound = 50
 crossover_rate = .7
 PM = .7
 chipperText = chipperText.replace(" ","")
-F = D/3
+F = random.uniform(0,2)
 cr = D/2
 #F = 1
 f0 = .0
@@ -27,43 +27,43 @@ m3 = 26
 gene = 100
 
 def _calc_ngram(text,n):
-	prob_result = {}
-	results = calc_ngram(text,n)
-	text_len = len(results)
-	i = 0
-	for x in results:
-		key = str(x[0]).lower()
-		prob_result[key] = x[1]/float(text_len)
-		#print key,' : ', prob_result[key]
-		i += 1
-	return prob_result
+    prob_result = {}
+    results = calc_ngram(text,n)
+    text_len = len(results)
+    i = 0
+    for x in results:
+        key = str(x[0]).lower()
+        prob_result[key] = x[1]/float(text_len)
+        #print key,' : ', prob_result[key]
+        i += 1
+    return prob_result
 
 def _get_english_bigram():
-	with open('english_bigram.txt') as f:
-		_file = f.read().splitlines()
+    with open('english_bigram.txt') as f:
+        _file = f.read().splitlines()
 
-	bigram = {}
-	
-	for row in _file:
-		row = row.split()
-		key = str(row[0].lower())
-		bigram[key] = round(float(row[1]) / 4324127906, 7)
+    bigram = {}
+    
+    for row in _file:
+        row = row.split()
+        key = str(row[0].lower())
+        bigram[key] = round(float(row[1]) / 4324127906, 7)
 
-	return bigram
+    return bigram
 
 def _get_english_trigram():
-	with open('english_trigram.txt') as f:
-		_file = f.read().splitlines()
+    with open('english_trigram.txt') as f:
+        _file = f.read().splitlines()
 
-	trigram = {}
-	
-	for row in _file:
-		row = row.split()
-		key = str(row[0].lower())
-		trigram[key] = round(float(row[1]) / 4274127909, 7)
-		# print key,' - ',row[1],' - ',trigram[key]
+    trigram = {}
+    
+    for row in _file:
+        row = row.split()
+        key = str(row[0].lower())
+        trigram[key] = round(float(row[1]) / 4274127909, 7)
+        # print key,' - ',row[1],' - ',trigram[key]
 
-	return trigram
+    return trigram
 
 def _get_english_monogram():
     with open('english_monogram.txt') as f:
@@ -85,163 +85,146 @@ trigram_list = _get_english_trigram()
 monogram_list = _get_english_monogram()
 
 def get_decryptText(pop, chipperText):
-	DecText = [[0 for i in range(len(chipperText))] for j in range(len(pop))] 
-	for i in range(len(pop)): 
-		for j in range(len(chipperText)): 
-			idx = (j / D) * D + pop[i][j % D] 
-			
-			DecText[i][idx] = chipperText[j] #wiih keren
-		#print '-->',''.join(DecText[i])
-	return DecText
+    DecText = [[0 for i in range(len(chipperText))] for j in range(len(pop))] 
+    for i in range(len(pop)): 
+        for j in range(len(chipperText)): 
+            idx = (j / D) * D + pop[i][j % D] 
+            DecText[i][idx] = chipperText[j] 
+        #print '-->',''.join(DecText[i])
+    return DecText
 
 def get_decryptANS(pop,an, chipperText):
-	DecANS = [0 for i in range(len(chipperText))] 
-	for i in range(len(chipperText)): 
-			idx = (i / D) * D + pop[an][i % D] 
-			
-			DecANS[idx] = chipperText[i] #wiih keren
+    DecANS = [0 for i in range(len(chipperText))] 
+    for i in range(len(chipperText)): 
+        idx = (i / D) * D + pop[an][i % D] 
+        DecANS[idx] = chipperText[i] 
         print '-->',''.join(DecANS)
-	return DecANS
+    return DecANS
 
 def get_n_gram(DecText,n):
-	nGramProb = [0 for i in range(len(pop))]
-	for i in range(len(pop)):
+    nGramProb = [0 for i in range(len(pop))]
+    for i in range(len(pop)):
+        decText = ''.join(DecText[i])
+        # print decText
 
-		decText = ''.join(DecText[i])
-		# print decText
-
-		nGramProb[i] = _calc_ngram(decText,n)
-		
-		#print 'DecText :',decText		
-		#print 'Prob : ', nGramProb[i],'\n'	
-	return nGramProb
+        nGramProb[i] = _calc_ngram(decText,n)
+        
+        #print 'DecText :',decText      
+        #print 'Prob : ', nGramProb[i],'\n' 
+    return nGramProb
 
 def get_bestBig(bigramProb,n):
-	bestBig = [0 for i in range(len(pop))]
-	for i in range(len(pop)): 
-		bestBig[i] = dict(bigramProb[i].items()[:n])
-	return bestBig
+    bestBig = [0 for i in range(len(pop))]
+    for i in range(len(pop)): 
+        bestBig[i] = dict(bigramProb[i].items()[:n])
+    return bestBig
 
 
 def calculate_fitness(monogramProb, bigramProb, trigramProb):
-	fitness = [0 for _ in range(len(pop))]
+    fitness = [0 for _ in range(len(pop))]
 
-	bestBigram = get_bestBig(bigramProb,b1)
-	bestTrigram = get_bestBig(trigramProb,t2)
-        bestMonogram = get_bestBig(monogramProb,m3)
+    bestBigram = get_bestBig(bigramProb,b1)
+    bestTrigram = get_bestBig(trigramProb,t2)
+    bestMonogram = get_bestBig(monogramProb,m3)
 
-	#sum_bigram = 0 
-        #sum_trigram = 0
-	for i in range(len(pop)):
-                sum_monogram = 0
-                sum_bigram = 0 
-                sum_trigram = 0
-		for (mo, bi, tri) in zip(bestMonogram[i], bestBigram[i], bestTrigram[i]):
-                        #sum_monogram = sum_monogram + monogram_list[mo] * bestMonogram[i][mo]
-                        #sum_bigram = sum_bigram + bigram_list[bi] * bestBigram[i][bi]
-		              	#sum_trigram = sum_trigram + trigram_list[tri] * bestTrigram[i][tri]
-			
-                        sum_monogram = sum_monogram + abs(monogram_list[mo] - bestMonogram[i][mo])
-                        sum_bigram = sum_bigram + abs(bigram_list[bi] - bestBigram[i][bi])
-                        sum_trigram = sum_trigram + abs(trigram_list[tri] - bestTrigram[i][tri])
+    #sum_bigram = 0 
+    #sum_trigram = 0
+    for i in range(len(pop)):
+        sum_monogram = 0
+        sum_bigram = 0 
+        sum_trigram = 0
+        for (mo, bi, tri) in zip(bestMonogram[i], bestBigram[i], bestTrigram[i]):
+            #sum_monogram = sum_monogram + monogram_list[mo] * bestMonogram[i][mo]
+            #sum_bigram = sum_bigram + bigram_list[bi] * bestBigram[i][bi]
+            #sum_trigram = sum_trigram + trigram_list[tri] * bestTrigram[i][tri]
+            
+            sum_monogram = sum_monogram + abs(monogram_list[mo] - bestMonogram[i][mo])
+            sum_bigram = sum_bigram + abs(bigram_list[bi] - bestBigram[i][bi])
+            sum_trigram = sum_trigram + abs(trigram_list[tri] - bestTrigram[i][tri])
 
-                fitness[i] = 1/((f0 * sum_monogram) + (f1 * sum_bigram) + (f2 * sum_trigram))
-                #print 'fitness individu ke - ', i, '(populasi ', pop[i], ') >>> ', fitness[i]
-	return fitness
+            fitness[i] = 1/((f0 * sum_monogram) + (f1 * sum_bigram) + (f2 * sum_trigram))
+            #print 'fitness individu ke - ', i, '(populasi ', pop[i], ') >>> ', fitness[i]
+    return fitness
 
 def get_r(i): #generate r1, r2, r3 buat mutasi
-	r = np.random.permutation(len(pop))
-	newR = [r[0],r[1],r[2]]
-
-	while (i in newR):
-		r = np.random.permutation(len(pop))
-		newR = [r[0],r[1],r[2]]
-	return newR
+    r = np.random.permutation(len(pop))
+    if (r[0] == i):
+        r[0] = r[3]
+    if (r[1] == i):
+        r[1] = r[3]
+    if (r[2] == i):
+        r[2] = r[3]
+            
+    newR = [r[0],r[1],r[2]]
+    return newR
 
 
 def get_best(pop_fitness):
-	min_fitness = pop_fitness[0]
-	min_index = 0
+    min_fitness = pop_fitness[0]
+    min_index = 0
 
-	for i in range(len(pop)):
-		if (pop_fitness[i] < min_fitness):
-			min_fitness = pop_fitness[i]
-			min_index = i
+    for i in range(len(pop)):
+        if (pop_fitness[i] < min_fitness):
+            min_fitness = pop_fitness[i]
+            min_index = i
 
-	return {'index': min_index, 'fitness':min_fitness}
+    return {'index': min_index, 'fitness':min_fitness}
 
 def fill(uPop, pop):
-	tmpPop = [pop[i] for i in range(len(pop))]
-	for i in range(len(uPop)):
-		if uPop[i] == -1:
-			for j in range(len(tmpPop)):
-				if (tmpPop[j] not in uPop) and (tmpPop[j] != -1):
-					uPop[i] = tmpPop[j]
-					tmpPop[j] = -1
-					break
-	return uPop
+    tmpPop = [pop[i] for i in range(len(pop))]
+    for i in range(len(uPop)):
+        if uPop[i] == -1:
+            for j in range(len(tmpPop)):
+                if (tmpPop[j] not in uPop) and (tmpPop[j] != -1):
+                    uPop[i] = tmpPop[j]
+                    tmpPop[j] = -1
+                    break
+    return uPop
 
 
-
-def mutasi(pop, pop_fitness):	
-        vPop = [[-1 for i in range(D)] for j in range(len(pop))]
-
-	ans = get_best(pop_fitness)
-	for i in range(len(pop)):
-                r0 = int(random.uniform(0,D-F))
-                rand = random.uniform(0,1)
-                rin = int(random.uniform(0,len(pop)))
-                #print 'rand:', rand
-                
-                #if (rand < PM):
-                #        for j in range(0,r0):
-                #                vPop[i][j] = pop[ans['index']][j]
-                #        vPop[i][r0] = pop[ans['index']][r0+F-1]
-                #        for k in range(r0+1,r0+F):
-                #                vPop[i][k] = pop[ans['index']][k-1]
-                #        for l in range(r0+F,D):
-                #                vPop[i][l] = pop[ans['index']][l]
-                if (rand < PM):
-                        for j in range(0,r0):
-                                vPop[i][j] = pop[rin][j]
-                        vPop[i][r0] = pop[rin][r0+F-1]
-                        for k in range(r0+1,r0+F):
-                                vPop[i][k] = pop[rin][k-1]
-                        for l in range(r0+F,D):
-                                vPop[i][l] = pop[rin][l]
-                #if (rand < PM):
-                #        for j in range(0,r0):
-                #                vPop[i][j] = pop[i][j]
-                #        vPop[i][r0] = pop[i][r0+F-1]
-                #        for k in range(r0+1,r0+F):
-                #                vPop[i][k] = pop[i][k-1]
-                #        for l in range(r0+F,D):
-                #                vPop[i][l] = pop[i][l]
-        	if (rand >= PM):
-                        for m in range(D):
-                                vPop[i][m] = pop[i][m]
-        return vPop
+def mutasi(pop, pop_fitness):   
+    vPop = [[-1 for i in range(D)] for j in range(len(pop))]        
+    ans = get_best(pop_fitness)
+    for i in range(len(pop)):
+        vv = [0 for ll in range(D)]
+        rand = random.uniform(0,1)
+        r = get_r(i)
+        r1 = r[0]
+        r2 = r[1]
+        r3 = r[2]
+        for j in range(D):
+            vv[j] = pop[r1][j] + F * (pop[r2][j] - pop[r3][j])
+        ii = 0
+        for k in range(D):
+            min = 0
+            for l in range(1,D):
+                if (vv[min] > vv[l]):
+                    min = l
+            vPop[i][min] = ii
+            vv[min] = 9999
+            ii = ii + 1
+        #print vPop[i]
+    return vPop
 
 
 def crossover(pop, mutasi_pop):
-	uPop = [ [-1 for i in range(D)] for j in range (len(pop))] #init dengan -1
-        rcr = [[-1 for i in range(cr)] for j in range (len(pop))]
-        for i in range(len(pop)):
-		r = np.random.permutation(D)
-                for m in range(cr):
-                        rcr[i][m] = r[m]
-                
-                rand = random.uniform(0,1)
-                if (rand < crossover_rate):
-                        for j in range(D):
-                                if i not in rcr:
-                                        uPop[i][j] = mutasi_pop[i][j]
-                        uPop[i] = fill(uPop[i], pop[i])
+    uPop = [ [-1 for i in range(D)] for j in range (len(pop))] #init dengan -1
+    for i in range(len(pop)):
+        r = int(random.uniform(0,D))
+        rand = random.uniform(0,1)
+        if (rand < crossover_rate):
+            for j in range(D):
+                uPop[i][j] = mutasi_pop[i][j]
                 if (rand >= crossover_rate):
-                        for nn in range(D):
-                                uPop[i][nn] = pop[i][nn]
-
-	return uPop        
+                    for nnn in range(D):
+                        uPop[i][nnn] = pop[i][nnn]
+                    for nn in range(D):
+                        if (nn == r):
+                            uPop[i][nn] = mutasi_pop[i][nn]
+                            for jj in range(D):
+                                if (pop[i][jj] == mutasi_pop[i][nn]):
+                                    uPop[i][jj] = pop[i][nn]
+    return uPop        
 
 for kk in range(0,10):
         print '========================================================'
@@ -255,7 +238,6 @@ for kk in range(0,10):
         jumPop = int(random.uniform(batas_bawah,batas_atas))
         print 'jumPop = ', jumPop
         pop = [np.random.permutation(D) for _ in range(jumPop)]
-        #print pop
         for i in range(jumPop):
                 for j in range(i):
                         if ((pop[i]==pop[j]).all()):
@@ -315,9 +297,9 @@ for kk in range(0,10):
                                 pop[i] = cross_pop[i]
                                 pop_fitness[i] = cross_pop_fitness[i]
                 ans = get_best(pop_fitness)
-                #	print 'generasi ke-',gen+1,' >>> ',pop[ans['index']],' | fitness : ',ans['fitness']
+                #   print 'generasi ke-',gen+1,' >>> ',pop[ans['index']],' | fitness : ',ans['fitness']
 
-	
+    
                 # ans = get_best(pop_fitness)
         print gen,' >>> ',pop[ans['index']],' | fitness : ',ans['fitness']
         print 'DECRYPTED TEXT:'
